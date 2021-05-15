70c635fd3de6e347f27ff9a5a9cafb3ae66862df
unknown <agustin15071997@gmail.com>
2021-05-14 21:35:10 -0300

    Creacion del boton vaciar carrito y opcion de poner cantidad

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,23 +1,36 @@
 const productos = [
-    { name: 'Zapato 1', price: 500 },
-    { name: 'Zapato 2', price: 100 },
-    { name: 'Zapato 3', price: 200 },
-    { name: 'Zapato 4', price: 300},
-    { name: 'Zapato 5', price: 700},
-    { name: 'Zapato 6', price: 700},
+  { name: "Zapato 1", price: 500 },
+  { name: "Zapato 2", price: 100 },
+  { name: "Zapato 3", price: 200 },
+  { name: "Zapato 4", price: 300 },
+  { name: "Zapato 5", price: 700 },
+  { name: "Zapato 6", price: 700 },
 ];
 let count = 0;
 let suma = 0;
-
-const carrito = [ ] ;
+let carrito = [];
 
 function agregar(a) {
+  console.clear();
+  let b = Number(document.getElementById("cant" + a).value);
+  for (let i = 0; i < b; i++) {
     carrito.push(productos[a]);
-    console.table(carrito);
     count++;
-    document.getElementById('count').innerHTML =  `${count} Items`;
-
     suma = suma + productos[a].price;
-    document.getElementById('total').innerHTML =  `Total: $${suma} pesos`;
+  }
+  console.table(carrito);
+  console.log('Zapatos en el carrito:',carrito.length);
+  document.getElementById("count").innerHTML = `${count} Items`;
+  document.getElementById("total").innerHTML = `Total: $${suma} pesos`;
+  document.getElementById("vaciar").classList.remove("hidden");
+}
 
+function vaciar() {
+  carrito = [];
+  count = 0;
+  suma = 0;
+  console.log('Zapatos en el carrito:',carrito.length);
+  document.getElementById("count").innerHTML = `Vacío`;
+  document.getElementById("total").innerHTML = ``;
+  document.getElementById("vaciar").classList.add("hidden");
 }
